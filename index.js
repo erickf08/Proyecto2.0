7a6fb5afb9ef4900e66d833eeca3a5aa5e9c403f
erickf08 <ef815095@gmail.com>
2025-02-02 22:45:29 -0800

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ yesBtn.addEventListener('click',function (){
 const noBtn = document.querySelector('#noBtn');
 
 noBtn.addEventListener('mouseover', function () {
-    const randomX = parseInt(Math.random()*1000);
-    const randomY = parseInt(Math.random()*1000);
+    const randomX = parseInt(Math.random()*500);
+    const randomY = parseInt(Math.random()*500);
     noBtn.style.setProperty('top',randomY+'%');
     noBtn.style.setProperty('left',randomX+'%');
     noBtn.style.setProperty('transform',`translate(-${randomX}%,-${randomY}%)`);
